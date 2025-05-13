9a798ec0dde6f8f2ab420d25646c5f57e8371658
shubham9049 <shubham9049>
2025-05-13 12:19:11 +0530

    add agent page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Landing } from "./pages/Landing";
+import AgentDetail from "./pages/AgentDetail";
+import AgentsSection from "./components/Agents";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/agents/:id" element={<AgentDetail />} />
+        <Route path="/AgentsSection" element={<AgentsSection />} />
       </Routes>
     </Router>
   );
